962d1f5f200882611bfa4e351b02eceb710870da
mergenthaler <mergenthaler@cqse.eu>
2020-10-21 16:09:59 +0200

    new branch

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator {
 
     public int add(int a, int b) {
     	// new branch one
-    	// new branch two
+    	// new branch five
     	// master
         int result = a;
         result += b;
